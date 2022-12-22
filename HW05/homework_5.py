d1c036d53af8e7f2604b7628eb1995f325d14de3
Vadym Demchenko <119288696+VadymDemchenko@users.noreply.github.com>
2022-12-22 21:25:02 +0200

    Update homework_5.py
    2 issues have been fixed. Waiting for your feedback

--- HW05/homework_5.py
+++ HW05/homework_5.py
@@ -1,25 +1,23 @@
 import time
 
 user_input = input("Введіть Ваш текст або число:\n")
-upper_text = 0
-lower_text = 0
-spaces = 0
 if user_input.isdigit():
     if int(user_input) % 2 == 0:
         print("Це парне число")
     else:
         print("Це непарне число")
-elif user_input.replace(' ', '').isalpha():
-
-    list_user_input = list(user_input)
-    for sign in list_user_input:
+elif all(x.isalpha() or x.isspace() for x in user_input):
+    upper_text = 0
+    lower_text = 0
+    spaces = 0
+    for sign in user_input:
         if sign.islower():
             lower_text += 1
         elif sign.isupper():
             upper_text += 1
         else:
-            continue
-    print(f"У вашому тексті {upper_text} літери у верхньому регістрі, {lower_text} літери у нижньому регістрі")
+            spaces += 1
+    print(f"У вашому тексті {upper_text} літери у верхньому регістрі, {lower_text} літери у нижньому регістрі та {spaces} пробілів")
 else:
     print('Ви ввели символ')
 
