cffa71674e0c3232cd00e5a9e54172377abbc00d
Vadym Demchenko <119288696+VadymDemchenko@users.noreply.github.com>
2022-12-25 12:21:51 +0200

    Update homework_5.py
    Sorry, it seems I didn't understand task right. Now it works in right way (I hope on it)
    Thanks for your feedback

--- HW05/homework_5.py
+++ HW05/homework_5.py
@@ -1,25 +1,22 @@
 import time
 
 user_input = input("Введіть Ваш текст або число:\n")
-if user_input.isdigit():
-    if int(user_input) % 2 == 0:
-        print("Це парне число")
-    else:
-        print("Це непарне число")
-elif all(x.isalpha() or x.isspace() for x in user_input):
-    upper_text = 0
-    lower_text = 0
-    spaces = 0
-    for sign in user_input:
-        if sign.islower():
-            lower_text += 1
-        elif sign.isupper():
-            upper_text += 1
+
+for sign in user_input:
+    if sign.isdigit():
+        if int(sign) % 2 == 0:
+            print(sign + " - це парне число")
         else:
-            spaces += 1
-    print(f"У вашому тексті {upper_text} літери у верхньому регістрі, {lower_text} літери у нижньому регістрі та {spaces} пробілів")
-else:
-    print('Ви ввели символ')
+            print(sign + " - це непарне число")
+    elif sign:
+        if sign.islower():
+            print(sign + " - це літера у нижньому регістрі")
+        elif sign.isupper():
+            print(sign + " - це літера у верхньому регістрі")
+        elif sign.isspace():
+            print(sign + " - це символ 'space'")
+        else:
+            print(sign + " - це символ")
 
 while True:
     print(" I love Python")
