76e59076087323ec2715c5fdff30b2ceb0b03557
Vadym Demchenko <vadimdemtchenko@gmail.com>
2023-01-16 23:53:45 +0200

    can't get the point of last necessary task in homework

---
+++ HW11/homework_11.py
@@ -0,0 +1,47 @@
+import time
+from datetime import datetime
+
+
+class ContextManager:
+    def __enter__(self):
+        print("="*10)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("="*10)
+
+
+with ContextManager():
+    try:
+        def name_time_decorator(func):
+            def inner1(*args, **kwargs):
+                print(f"Function {func.__name__} started at {datetime.now()}")
+                start_time = time.time()
+                func(*args, **kwargs)
+                end_time = time.time()
+                print(f"Function {func.__name__} take  {end_time - start_time}")
+            return inner1
+
+
+        @name_time_decorator
+        def multiply_two(num):
+            print(num * 2)
+
+        class MyCustomException(Exception):
+            def __init__(self, message):
+                self.message = message
+
+
+        inp = input("Input some integer:")
+
+        try:
+            if not inp.isdigit():
+                raise MyCustomException("Custom exception is occurred")
+        except MyCustomException as exc:
+            print(exc)
+        print("smth else")
+    except Exception as error:
+        print(error)
+
+
+
