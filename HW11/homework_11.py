32820565004130fa977b3cacf1cfb6279517a651
Vadym Demchenko <vadimdemtchenko@gmail.com>
2023-02-03 17:06:05 +0200

    now context manager will print info about exception. result of the function is calling after context manager

--- HW11/homework_11.py
+++ HW11/homework_11.py
@@ -1,49 +1,61 @@
-import time
 from datetime import datetime
 
 
 class ContextManager:
     def __enter__(self):
         print("="*10)
-        return self
+        return True
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print("="*10)
+        if exc_tb is None and exc_type is None and exc_val is None:
+            print("=" * 10)
+        else:
+            print(exc_type)
+            print(exc_val)
+            print(exc_tb)
+            print("=" * 10)
+            return True
 
 
 with ContextManager():
+    print(1/0)
+
+    def name_time_decorator(func):
+        def inner1(*args, **kwargs):
+            print(f"Function {func.__name__} started at {datetime.now()}")
+            func(*args, **kwargs)
+        return inner1
+
+
+    @name_time_decorator
+    def multiply_two(num):
+        print(num * 2)
+
+
+    result = multiply_two
+
+
+    class MyCustomException(Exception):
+        def __init__(self, message):
+            self.message = message
+
+
+    inp = input("Input some integer:")
+
     try:
-        def name_time_decorator(func):
-            def inner1(*args, **kwargs):
-                print(f"Function {func.__name__} started at {datetime.now()}")
-                start_time = time.time()
-                func(*args, **kwargs)
-                end_time = time.time()
-                print(f"Function {func.__name__} take  {end_time - start_time}")
-            return inner1
+        if not inp.isdigit():
+            raise MyCustomException("Custom exception is occurred")
+    except MyCustomException as exc:
+        print(exc)
+    print("smth else")
 
-
-        @name_time_decorator
-        def multiply_two(num):
-            print(num * 2)
-
-        multiply_two(5)
-
-        class MyCustomException(Exception):
-            def __init__(self, message):
-                self.message = message
-
-
-        inp = input("Input some integer:")
-
-        try:
-            if not inp.isdigit():
-                raise MyCustomException("Custom exception is occurred")
-        except MyCustomException as exc:
-            print(exc)
-        print("smth else")
-    except Exception as error:
-        print(error)
-
-
-
+try:
+    print("="*10)
+    result(5)
+except NameError:
+    print("Seems like name is not found")
+else:
+    print("Here is your result!")
+finally:
+    print("Thanks for using this func!")
+    print("="*10)
